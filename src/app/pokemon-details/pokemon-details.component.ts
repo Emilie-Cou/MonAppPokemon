307d62b7e590e8219551f0a6729826781940a618
Coubeau Emilie <e.coubeau@stag.technofuturtic.education>
2023-09-13 00:07:18 +0200

    reglage affichage

--- src/app/pokemon-details/pokemon-details.component.ts
+++ src/app/pokemon-details/pokemon-details.component.ts
@@ -17,8 +17,21 @@ export class PokemonDetailsComponent {
   ) {}
 
   ngOnInit () {
-    this._nomPokeService.getById(0).subscribe({
-
+    this._nomPokeService.getById(25).subscribe({
+      next : (data) => {
+        this.monPoke = {
+          name : data.name,
+          id : data.id,
+          type : data.type,
+          order : data.order,
+          height : data.height,
+          weight : data.weight,
+          base_experience : data.base_experience,
+          is_default : data.is_default,
+          statistique : data.statistique,
+        }
+        console.log(this.monPoke);
+      }
     })
   }
 
